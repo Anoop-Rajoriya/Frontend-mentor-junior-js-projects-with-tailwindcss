8bfac87513770d8a5157c11dd9e4f0d8d12cd1d0
Anoop Rajoriya <anooprajoriya9827@gmail.com>
2024-05-08 20:57:38 +0530

    age calculator ui completed

--- Age calculator app/tailwind.config.js
+++ Age calculator app/tailwind.config.js
@@ -1,15 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 const plugin = require('tailwindcss/plugin')
-
 module.exports = {
   content: ["./*.html"],
   theme: {
     extend: {},
   },
   plugins: [
-    plugin(function({ addVariant }) {
+    plugin(function ({ addVariant }) {
       addVariant("error", ".error &");
-    })
+    }),
   ],
-}
-
+};
