09361abcef35491e1a347fc0460404cf0883d182
Anoop Rajoriya <anooprajoriya9827@gmail.com>
2024-05-15 21:42:46 +0530

    ui almoste done

--- notifications page main/tailwind.config.js
+++ notifications page main/tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin')
 module.exports = {
   content: ["./*.{html, js}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addVariant }) {
+      addVariant("notRead", ".notRead&");
+      addVariant("hasMessage", ".hasMessage&");
+    }),
+  ],
 }
 
